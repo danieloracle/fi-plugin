ca98e454e86dc7942e017f82b539ddfbb4f4f137
Daniel Goldenberg <daniel.aron@goldenberg.co>
2022-03-10 14:44:27 -0300

    add data example

--- FileCabinet/SuiteApps/com.netsuite.fiplugin/fi_iframe_example.js
+++ FileCabinet/SuiteApps/com.netsuite.fiplugin/fi_iframe_example.js
@@ -5,6 +5,18 @@
 
 define(['N/ui/serverWidget', 'N/runtime', 'N/record', 'N/search', 'N/url', 'N/http', 'N/file'],
     function(ui, runtime, record, search, url, http, file) {
+        function getBAI(){
+            var downloadedData =
+            "chr01,BANKOFAMERICA,cas72068,181108,1330,1,80,1,2\n"+
+            "02,cas72068,071000039,1,190109,2359,,2\n"+
+            "03,9999999999,USD\n"+
+            "16,165,25000,Z,965710090000149,004646894728\n"+
+            "88,ME CTRL DIS 004646894728 CR\n"+
+            "49,25000,4\n"+
+            "98,25000,1,6\n"+
+            "99,25000,1,8"
+            return downloadedData;
+        }
 
         function onRequest(context) {
             // Show iFrame link
@@ -18,22 +30,18 @@ define(['N/ui/serverWidget', 'N/runtime', 'N/record', 'N/search', 'N/url', 'N/ht
                 hideNavBar: true
             });
 
-            html = '<h1>Welcome</h1><h2>User already authenticated</h2>';
+            html = '<h1>Welcome</h1>';
+            // html = html + '<h2>User already authenticated</h2>';
             // Show iFrame
-            html = html + '<a href="'+output+'">iFrame</a>';
+            html = html + '<h2>iFrame example</h2>';
+            html = html + '<a href="'+output+'">iFrame URL</a>';
             html = html + '<br> <br>';
             // Show bai2 file content
-            try{
-                var file_id = '/SuiteApps/com.netsuite.fiplugin/src/bai2.txt';
-                var fileObj = file.load({id: file_id});
-                var c = fileObj.getContents();
-                html = html + "<br>";
-                html = html  + "<div>"+JSON.stringify(c)+"</div>";
-            }catch(e){
-                log.error("load file error", e);
-                html = html  + "<div>"+JSON.stringify(e)+"</div>";
-            }
-            
+            html = html + '<h2>BAI2 file example content to import</h2>';
+            html = html + '<div>';
+            html = html + getBAI();
+            html = html + '</div>';
+
             var htmlContent = form.addField({
                 id: 'custpage_htmlfield',
                 type: ui.FieldType.INLINEHTML,
--- FileCabinet/SuiteApps/com.netsuite.fiplugin/fi_plugin_example.js
+++ FileCabinet/SuiteApps/com.netsuite.fiplugin/fi_plugin_example.js
@@ -25,22 +25,55 @@ define(['N/https', 'N/search', 'N/url'],
         {
             log.debug("getAccounts", "Getting customers accounts from Bank");
             context.addAccount({
-                accountMappingKey: "CHECKING 0002153852",
-                displayName: "Checking (XXXX12)",
+                accountMappingKey: "CHECKING 000000001",
+                displayName: "Checking (XXXX01)",
                 accountType: "BANK",
                 currency: "USD",
                 groupName: "Oracle Bank",
                 lastUpdated: "2022-03-08T01:23:45"
             });
+            context.addAccount({
+                accountMappingKey: "CHECKING 222222222",
+                displayName: "Checking (XXXX22)",
+                accountType: "BANK",
+                currency: "USD",
+                groupName: "Oracle Bank",
+                lastUpdated: "2022-03-08T01:23:45"
+            });
+
         }
 
         function getTransactionData(context)
         {
-            var downloadedData =
-                "TYPE;DATE;NAME;MEMO;AMOUNT\n"+
-                "CHECK;2017-04-13;Jessica L Sikes;;-1228.98\n" +
-                "CHECK;2017-04-13;Tom A Taylor;;-555.97";
+            // // Example 1: CSV/json example
+            // // ---------------------------
+            // var downloadedData =
+            //     "TYPE;DATE;NAME;MEMO;AMOUNT\n"+
+            //     "CHECK;2017-04-13;Jessica L Sikes;;-1228.98\n" +
+            //     "CHECK;2017-04-13;Tom A Taylor;;-555.97";
+            //
+            // var accountRequests = JSON.parse(context.accountRequestsJSON);
+            // if (accountRequests != null) {
+            //     accountRequests.forEach(function (accountRequest) {
+            //         var accountId = accountRequest.accountMappingKey;
+            //         var fromDateTime = accountRequest.dataStartTime;
+            //         var toDateTime = accountRequest.dataEndTime;
+            //         log.debug("accountRequests.forEach", accountId + " - " + fromDateTime + " - " + toDateTime);
+            //         context.addDataChunk({dataChunk: downloadedData});
+            //     });
+            // }
 
+            // Example 2: BAI2 file
+            // ------------
+            var downloadedData =
+            "chr01,BANKOFAMERICA,cas72068,181108,1330,1,80,1,2\n"+
+            "02,cas72068,071000039,1,190109,2359,,2\n"+
+            "03,9999999999,USD\n"+
+            "16,165,25000,Z,965710090000149,004646894728\n"+
+            "88,ME CTRL DIS 004646894728 CR\n"+
+            "49,25000,4\n"+
+            "98,25000,1,6\n"+
+            "99,25000,1,8";
             context.addDataChunk({dataChunk: downloadedData});
             context.returnAccountRequestsJSON({accountsJson: context.accountRequestsJSON});
         }
