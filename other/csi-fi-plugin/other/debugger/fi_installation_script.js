5519f76428cafa6f51162325955b25994fd3564b
Daniel Goldenberg <daniel.aron@goldenberg.co>
2022-03-30 20:22:54 -0300

    v1 fi_installation_script ok

--- other/csi-fi-plugin/other/debugger/fi_installation_script.js
+++ other/csi-fi-plugin/other/debugger/fi_installation_script.js
@@ -2,7 +2,7 @@
  * @NApiVersion 2.x
  */
 
- require (['N/search', 'N/record'], function (search, record) {
+ require (['N/search', 'N/query', 'N/record'], function (search, query, record) {
 
     var fi_name = 'Oracle Bank Scripted';
     var fi_description = 'Oracle Bank Scripted';
@@ -13,6 +13,7 @@
     
 
 
+    //Check if exists FINANCIAL_INSTITUTION
     var fi_id = -1;
     var mySearch = search.create({
         type: search.Type.FINANCIAL_INSTITUTION,
@@ -53,6 +54,95 @@
             fieldId: 'description',
             value: fi_description
         }).save();
-        log.debug(newFIRecord);
-    }
+        fi_id = newFIRecord;
+        log.debug('new fi_id', fi_id);
+
+
+    //GET FI_CONNECTIVITY_PLUGIN ID:
+    var fiPluginQuery = query.create({
+        type: query.Type.FI_CONNECTIVITY_PLUGIN
+    });
+
+    fiPluginQuery.columns = [
+        fiPluginQuery.createColumn({
+            fieldId: 'id'
+        }),
+        fiPluginQuery.createColumn({
+            fieldId: 'name'
+        }),
+        fiPluginQuery.createColumn({
+            fieldId: 'scriptid'
+        })
+    ];
+
+    fiPluginQuery.condition = fiPluginQuery.createCondition({
+        fieldId: 'scriptid',
+        operator: query.Operator.IS,
+        values: 'customscript_fi_plugin_example'
+    });
+    var results = fiPluginQuery.run().asMappedResults();
+    var fi_connectivity_plugin_id = results[0].id;
+    log.debug('fi_connectivity_plugin_id', fi_connectivity_plugin_id);
+
+
+    //GET PARSER PLUGIN
+    var fiPluginQuery = query.create({
+        type: query.Type.ALL_PARSER_PLUGIN
+    });
+
+    fiPluginQuery.columns = [
+        fiPluginQuery.createColumn({
+            fieldId: 'id'
+        }),
+        fiPluginQuery.createColumn({
+            fieldId: 'name'
+        }),
+        fiPluginQuery.createColumn({
+            fieldId: 'scriptid'
+        })
+    ];
+
+    fiPluginQuery.condition = fiPluginQuery.createCondition({
+        fieldId: 'scriptid',
+        operator: query.Operator.IS,
+        values: 'customscript_bsp_parser_bai2'
+    });
+    var results = fiPluginQuery.run().asMappedResults();
+    var fi_parser_plugin_id = results[0].id;
+    log.debug('fi_parser_plugin_id', fi_parser_plugin_id);
+
+    //CREAT FORMAT PROFILE
+    log.debug('-- Create FORMAT_PROFILE --', 'hia');
+    var newFPRecord = record.create({
+        type: record.Type.FORMAT_PROFILE,
+        isDynamic: true
+    });
+    newFPRecord.setValue({
+        fieldId: 'formatprofile',
+        value: fp_name
+    });
+    newFPRecord.setValue({
+        fieldId: 'financialinstitution',
+        value: fi_id
+    });
+    newFPRecord.setValue({
+        fieldId: 'transactionparser',
+        value: fi_parser_plugin_id
+    });
+    newFPRecord.setValue({
+        fieldId: 'connectivitymethod',
+        value: fi_connectivity_plugin_id
+    });
+    newFPRecord.setValue({
+        fieldId: 'description',
+        value: fp_description
+    });
+    var fpID = newFPRecord.save();
+    log.debug('-- FORMAT_PROFILE --', fpID);
+
+
+
+}
+
+
 });
